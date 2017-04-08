cc7b723b0555fbf19af558e0cf0f3d678a9aa75b
Pedro Ladaria <pedro.ladaria@gmail.com>
2017-04-08 13:08:37 +0200

    update to emojione 2.2.7

--- README.md
+++ README.md
@@ -2,11 +2,21 @@
 
 Just a json file with a list of all emoji shortames. Sorted and grouped by category.
 
+Data extracted from [emojione](https://emojione.com) 2.2.7
+
 ## Install
 
 ```bash
 npm install emoji-shortnames
 ```
+
+## Build
+
+```bash
+# clone repo, then
+npm run build
+```
+
 
 ## License
 
---
+++ build.js
@@ -0,0 +1,13 @@
+const {readFileSync, writeFileSync} = require('fs');
+const {join} = require('path');
+
+const data = require('./data.json');
+
+const json = Object.keys(data).reduce((res, key) => {
+    const {category, shortname} = data[key];
+    res[category] = res[category] || [];
+    res[category].push(shortname);
+    return res;
+}, {});
+
+writeFileSync(join(__dirname, './emoji-shortnames.json'), JSON.stringify(json, null, '  '));
